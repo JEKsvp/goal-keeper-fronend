92cb1b39489dd69f745dbb18621a4948f91d49b9
JEKsvp <abadeksvp@gmail.com>
2018-10-28 01:30:14 +0400

    add onsen

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template id="main-page">
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">Title</div>
+    </v-ons-toolbar>
+
+    <p style="text-align: center">
+      <v-ons-button @click="$ons.notification.alert('Hello World!')">
+        Click me!
+      </v-ons-button>
+    </p>
+  </v-ons-page>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 import Vue from 'vue'
 import App from './App.vue'
+import VueOnsen from 'vue-onsenui';
 
 Vue.config.productionTip = false
+Vue.use(VueOnsen); // VueOnsen set here as plugin to VUE. Done automatically if a call to window.Vue exists in the startup code.
 
 new Vue({
   render: h => h(App)
