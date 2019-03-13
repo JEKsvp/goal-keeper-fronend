251da8750b45af5df80ae6be9de637b66f18350b
JEKsvp <abadeksvp@gmail.com>
2019-03-14 01:07:08 +0400

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,19 @@
 <template id="main-page">
-  <v-ons-page>
-    <v-ons-toolbar>
-      <div class="center">Title</div>
-    </v-ons-toolbar>
-
-    <p style="text-align: center">
-      <v-ons-button @click="$ons.notification.alert('Hello World!')">
-        Click me!
-      </v-ons-button>
-    </p>
-  </v-ons-page>
+  <v-app>
+    <v-container>
+        <v-layout align-center>
+            <v-btn color="success">Success</v-btn>
+        </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
-
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
-import VueOnsen from 'vue-onsenui';
 
 Vue.config.productionTip = false
-Vue.use(VueOnsen); // VueOnsen set here as plugin to VUE. Done automatically if a call to window.Vue exists in the startup code.
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount('#app')
