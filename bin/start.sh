5ca1f7a0a76ee71e44cf96c66d562464e6f2feb9
jeksvp <abadeksvp@gmail.com>
2020-07-26 01:23:43 +0300

    remove build step from start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 display_usage() {
-  echo -e "\n This script build docker image and start container for it-landscape-web"
+  echo -e "\n This script starts container for it-landscape-web"
   echo -e "\nUSAGE:\n\n ${0} <build-tag>\n"
 }
 
@@ -9,12 +9,6 @@ if [ $# -lt 1 ]; then
   display_usage
   exit 1
 fi
-
-SCRIPT_PATH=$(
-  cd "$(dirname "${BASH_SOURCE[0]}")" || exit
-  pwd -P
-)
-"${SCRIPT_PATH}"/build.sh "$1"
 
 docker run -e REDIS_HOST="docker.for.mac.host.internal" \
 -e BACKEND_URL="http://docker.for.mac.host.internal:8081" \
